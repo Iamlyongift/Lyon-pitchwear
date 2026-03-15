5cb84dd3ad472a9a2d3365591586959a91ad48ac
iam_lyongift <chukwumagift6036@gmail.com>
2026-03-16 00:41:31 +0100

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,15 @@ import express, { Request, Response, NextFunction } from "express";
 import createError from "http-errors";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
-import cors from 'cors';
+import cors from "cors";
 
 import adminRouter from "./routes/adminRouter";
-import {userRouter, authRouter} from "./routes/userRouter";
+import { userRouter, authRouter } from "./routes/userRouter";
 import productRouter from "./routes/productRouter";
-import orderRouter from './routes/orderRouter';
-import reviewRouter from './routes/reviewRouter';
-
-
+import orderRouter from "./routes/orderRouter";
+import reviewRouter from "./routes/reviewRouter";
 
 const app = express();
-
 
 // Middleware
 app.use(logger("dev"));
@@ -21,17 +18,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  }),
+);
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error("MIDDLEWARE ERROR:", err.message);
+  res.status(500).json({ success: false, message: err.message });
+});
+
 // Routes
 app.use("/api/admin", adminRouter);
 app.use("/api/user", userRouter);
-app.use("/api/auth", authRouter)
+app.use("/api/auth", authRouter);
 app.use("/api/products", productRouter);
-app.use('/api/orders', orderRouter);
-app.use('/api/reviews', reviewRouter);
+app.use("/api/orders", orderRouter);
+app.use("/api/reviews", reviewRouter);
 
 // 404 handler
 app.use((req: Request, res: Response, next: NextFunction) => {
@@ -39,16 +43,14 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 });
 
 // Global error handler
-app.use(
-  (err: any, req: Request, res: Response, next: NextFunction) => {
-    const status = err.status || 500;
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  const status = err.status || 500;
 
-    res.status(status).json({
-      success: false,
-      message: err.message || "Internal Server Error",
-      stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
-    });
-  }
-);
+  res.status(status).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+    stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
+  });
+});
 
 export default app;
